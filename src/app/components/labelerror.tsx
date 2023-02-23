789065798df5a584f155cb7e74e5398feaa8aa17
Benjy <oguntoyebenjamin2@gmail.com>
2023-02-23 10:07:38 +0000

    Added prettier and format scripts

--- src/app/components/labelerror.tsx
+++ src/app/components/labelerror.tsx
@@ -1,29 +1,34 @@
-"use client";
-import React, { useState } from "react";
-import styles from "./page.module.css";
-import { Inter } from "@next/font/google";
+'use client';
+import React, { useState } from 'react';
+import styles from './page.module.css';
+import { Inter } from '@next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
-interface ChildcomponentProps{
-    errorPasswordSet:string;
-    errorEmailSet:string;
-    errorUsernameSet:string;
-    errorMainSet:string;
+interface ChildcomponentProps {
+    errorPasswordSet: string;
+    errorEmailSet: string;
+    errorUsernameSet: string;
+    errorMainSet: string;
 }
 
-const Error: React.FC<ChildcomponentProps> = (props) =>{
-    console.log(props.errorEmailSet,props.errorMainSet,props.errorPasswordSet,props.errorUsernameSet,"hello")
+const Error: React.FC<ChildcomponentProps> = (props) => {
+    console.log(
+        props.errorEmailSet,
+        props.errorMainSet,
+        props.errorPasswordSet,
+        props.errorUsernameSet,
+        'hello',
+    );
     return (
-    <form>
-        <label className="text-red-500">
-       {props.errorEmailSet}
-       {props.errorPasswordSet}
-       {props.errorUsernameSet}
-       {props.errorMainSet};
-       error
-        </label>
-    </form>
-    )
-}
+        <form>
+            <label className="text-red-500">
+                {props.errorEmailSet}
+                {props.errorPasswordSet}
+                {props.errorUsernameSet}
+                {props.errorMainSet}; error
+            </label>
+        </form>
+    );
+};
 export default Error;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,20 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
-import Link from "next/link";
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from './page.module.css';
+import Link from 'next/link';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <main>
-      <div className="bg-black text-white">Hello Baba Scheduler Fam
-      <br></br>
-      <Link href="/login">
-      Login
-    </Link>
-    <br></br>
-    <Link href="/signup">
-    sign up 
-    </Link>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            <div className="bg-black text-white">
+                Hello Baba Scheduler Fam
+                <br></br>
+                <Link href="/login">Login</Link>
+                <br></br>
+                <Link href="/signup">sign up</Link>
+            </div>
+        </main>
+    );
 }
